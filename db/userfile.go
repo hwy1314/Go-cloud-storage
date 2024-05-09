238e5e27defd7dd6a92df22dbe7a7ef78f858351
hwy1314 <120362432+hwy1314@users.noreply.github.com>
2024-05-09 19:38:11 +0800

    Modified the defer statement

--- db/userfile.go
+++ db/userfile.go
@@ -6,28 +6,28 @@ import (
 	"time"
 )
 
-// UserFile: 用户文件表结构体
+//UserFile: 用户文件表结构体
 type UserFile struct {
-	UserName    string
-	FileHash    string
-	FileName    string
-	FileSize    int64
-	UploadAt    string
+	UserName string
+	FileHash string
+	FileName string
+	FileSize int64
+	UploadAt string
 	LastUpdated string
 }
 
-// OnUserFileUploadFinished: 更新用户文件表
-func OnUserFileUploadFinished(username, filehash, filename string, filesize int64) bool {
-	stmt, err := mydb.DBConn().Prepare(
+//OnUserFileUploadFinished: 更新用户文件表
+func OnUserFileUploadFinished(username,filehash,filename string,filesize int64) bool {
+	stmt,err := mydb.DBConn().Prepare(
 		"insert ignore into tbl_user_file (`user_name`,`file_sha1`,`file_name`," +
 			"`file_size`,`upload_at`) values (?,?,?,?,?)")
-	defer stmt.Close()
 	if err != nil {
 		fmt.Println(err.Error())
 		return false
 	}
+	defer stmt.Close()
 
-	_, err = stmt.Exec(username, filehash, filename, filesize, time.Now())
+	_,err = stmt.Exec(username,filehash,filename,filesize,time.Now())
 	if err != nil {
 		fmt.Println(err.Error())
 		return false
@@ -35,15 +35,15 @@ func OnUserFileUploadFinished(username, filehash, filename string, filesize int6
 	return true
 }
 
-// QueryUserFileMetas: 批量获取用户文件信息
-func QueryUserFileMetas(username string, limit int) ([]UserFile, error) {
+//QueryUserFileMetas: 批量获取用户文件信息
+func QueryUserFileMetas(username string,limit int) ([]UserFile,error) {
 	stmt, err := mydb.DBConn().Prepare(
 		"select file_sha1,file_name,file_size,upload_at,last_update from" +
 			" tbl_user_file where user_name=? limit ?")
-	defer stmt.Close()
 	if err != nil {
 		return nil, err
 	}
+	defer stmt.Close()
 
 	rows, err := stmt.Query(username, limit)
 	if err != nil {
@@ -60,8 +60,8 @@ func QueryUserFileMetas(username string, limit int) ([]UserFile, error) {
 			break
 		}
 
-		userFiles = append(userFiles, ufile)
+		userFiles = append(userFiles,ufile)
 	}
 
-	return userFiles, nil
+	return userFiles,nil
 }
